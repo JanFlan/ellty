c871511316dfdaf909fb9a88ea043152391eea54
Mohamed Badawy <mohamed.hesham.badawy@gmail.com>
2025-11-10 01:40:42 +0200

    chore: Set up project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <div className="app-container">
+        App content
+      </div>
+    </div>
+  )
+}
+
+export default App
+
